c4ce23ed0383cbc5dc7585b6e71ba8908508d864
NicholasMinardi25 <nicholasm204@nycstudents.net>
2025-02-25 20:07:45 -0500

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,11 @@
+def addition():
+    a = 5
+    b = 7
+    sum = a + b
+    print (f"we can also add variables like a and b. a being {a} and b being {b}")
+    print (f"the sum of a and b is {sum}")
+
+def intro():
+    print("Welcome to my first project! We learn to count to 20: ")
+    for i in range (21):
+        print({i})
